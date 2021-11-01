5bfb054725a6faef2bf49bbad684bdb1eda865e9
Alireza <leo.faridar@gmail.com>
2021-11-01 10:13:21 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+first_name = input('what is your name azizam???')
+# take your first name
+last_name = input('and tell me your last name pls!')
+# take your last name
+print('Hello', first_name + ' ' + last_name)
+# say hello to user
+age = input('how old are you honey???')
+# take user's age
+print('oh you are too young and fresh', first_name + ' ' + last_name)
+# print sth related to age
+more = input('are you currently single?')
+# take single or in rel
+if more != 'no':
+    print("that's sad hope you find someone!")
+else:
+    print('that is nice have fun with your love')
